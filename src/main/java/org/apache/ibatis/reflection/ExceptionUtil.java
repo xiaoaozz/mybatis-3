9f3763238e952934c7fb58ecba7f7a12b7c1c982
xiaoaozz <3189137314@qq.com>
2023-05-08 21:18:47 +0800

    异常拆包工具ExceptionUtil源码注释

--- src/main/java/org/apache/ibatis/reflection/ExceptionUtil.java
+++ src/main/java/org/apache/ibatis/reflection/ExceptionUtil.java
@@ -27,15 +27,24 @@ public class ExceptionUtil {
     // Prevent Instantiation
   }
 
+  /**
+   * 拆解InvocationTargetException和UndeclaredThrowableException异常的包装，从而得到被包装的真正异常。
+   * 思考一下为什么要给这两个异常拆包？
+   *
+   * @param wrapped 包装后的异常
+   * @return 拆解出的被包装异常
+   */
   public static Throwable unwrapThrowable(Throwable wrapped) {
-    Throwable unwrapped = wrapped;
+    Throwable unwrapped = wrapped; // 存放拆包得到的异常
     while (true) {
       if (unwrapped instanceof InvocationTargetException) {
+        // 拆包获得内部异常
         unwrapped = ((InvocationTargetException) unwrapped).getTargetException();
       } else if (unwrapped instanceof UndeclaredThrowableException) {
+        // 拆包获得内部异常
         unwrapped = ((UndeclaredThrowableException) unwrapped).getUndeclaredThrowable();
       } else {
-        return unwrapped;
+        return unwrapped; // 其他异常无需拆包
       }
     }
   }
